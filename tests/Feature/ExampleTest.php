825007dc481d74c36296243f73f1617b71a03916
Gihovani Filipp Pereira Demétrio <gihovani@gmail.com>
2018-05-14 18:02:01 -0300

    Adicionado arquivos de testes

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
